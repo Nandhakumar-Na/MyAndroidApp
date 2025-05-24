771d085ee14e7082c53b21f08359369787b6c32b
Nandhakumar-Na <nandhakumar2305022gmail.com>
2025-05-24 23:07:53 +0530

    zsxdcf

--- app/src/main/java/com/example/gitrepo/MainActivity.kt
+++ app/src/main/java/com/example/gitrepo/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GitRepoTheme {
-        Greeting("Android")
+        Greeting("Android Developer")
     }
 }
